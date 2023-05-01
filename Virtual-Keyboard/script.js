adf6b2beb7dca58f48c7c7af7a867abe58394d40
Oleg Krivenkov <krivenkof@gmail.com>
2023-05-01 14:45:48 +0300

    feat: add in html new eltments:title,description,language,conteiner and in css styles for them

--- Virtual-Keyboard/script.js
+++ Virtual-Keyboard/script.js
@@ -1,29 +1,53 @@
 const body = document.querySelector('body');
+
+// добавляем контейнер
+const conteiner = document.createElement('div');
+conteiner.setAttribute('class', 'conteiner');
+document.body.appendChild(conteiner);
+
+const title = document.createElement('p');
+title.setAttribute('class', 'title');
+const titleText = document.createTextNode("RSS Виртуальная клавиатура");
+title.appendChild(titleText)
+conteiner.appendChild(title);
+
 // добавляем поле ввода текста
 const textarea = document.createElement('textarea');
 textarea.setAttribute('id', 'textarea');
-body.appendChild(textarea);
+conteiner.appendChild(textarea);
 
 // добавляем контейнер для виртуальной клавиатуры
 const keyboardDiv = document.createElement('div');
 keyboardDiv.setAttribute('id', 'virtual-keyboard');
-document.body.appendChild(keyboardDiv);
+conteiner.appendChild(keyboardDiv);
+
+const description = document.createElement('p');
+description.setAttribute('class', 'description');
+const descriptionText = document.createTextNode("Клавиатура создана в операционной системе Windows");
+description.appendChild(descriptionText)
+conteiner.appendChild(description);
+
+const language = document.createElement('p');
+language.setAttribute('class', 'language');
+const languageText = document.createTextNode("Для переключения языка комбинация: левыe alt + shift");
+language.appendChild(languageText)
+conteiner.appendChild(language);
 
 function generateKeyboard() {
 // создаем массив клавиатуры
 const keyboardRows = [
-    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0','-','=','Backspace'],
-    ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'Del'],
-    ['CapsLock','a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"','Enter'],
-    ['Shift','z', 'x', 'c', 'v', 'b', 'n', 'm',',', '.','/','▲','Shift'],
-    ['Ctrl','Win', 'Alt', 'Space', 'Alt', 'Ctrl','▼','◄','►']
+['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0','-','=','Backspace'],
+['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'Del'],
+['CapsLock','a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"','Enter'],
+['Shift','z', 'x', 'c', 'v', 'b', 'n', 'm',',', '.','/','▲','Shift'],
+['Ctrl','Win', 'Alt', 'Space', 'Alt', 'Ctrl','▼','◄','►']
 ];
 const ruskeyboardRows = [
-    ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'],
-    ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ','Del'],
-    ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', '/', 'Enter'],
-    ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/','.','▲','Shift'],
-    ['Ctrl','Win', 'Alt', 'Space', 'Alt', 'Ctrl','▼','◄','►']
+['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'],
+['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ','Del'],
+['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', '/', 'Enter'],
+['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/','.','▲','Shift'],
+['Ctrl','Win', 'Alt', 'Space', 'Alt', 'Ctrl','▼','◄','►']
 ];
 
 const keyboardContainer = document.getElementById('virtual-keyboard');
@@ -43,3 +67,16 @@ keyboardContainer.appendChild(rowElement);
 });
 }
 generateKeyboard();
+
+// задаем размеры отдельных клавиш
+document.getElementsByClassName('keyboard-key')[13].style.width = "90px";
+document.getElementsByClassName('keyboard-key')[14].style.width = "70px";
+document.getElementsByClassName('keyboard-key')[27].style.width = "70px";
+document.getElementsByClassName('keyboard-key')[40].style.width = "103px";
+document.getElementsByClassName('keyboard-key')[41].style.width = "103px";
+document.getElementsByClassName('keyboard-key')[28].style.width = "103px";
+document.getElementsByClassName('keyboard-key')[56].style.width = "60px";
+document.getElementsByClassName('keyboard-key')[57].style.width = "383px";
+document.getElementsByClassName('keyboard-key')[58].style.width = "60px";
+document.getElementsByClassName('keyboard-key')[53].style.width = "103px";
+document.getElementsByClassName('keyboard-key')[59].style.width = "60px";
